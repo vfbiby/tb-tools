da234fa0ca019f3127f02145a37283f1ecdd8868
James Zhang <liulfbaby@gmail.com>
2024-09-17 00:25:59 +0800

    perfect type definitaion

--- src/contents/tb-shop-float.tsx
+++ src/contents/tb-shop-float.tsx
@@ -1,13 +1,48 @@
 import type {PlasmoCSConfig} from "plasmo"
+import {useEffect} from "react";
+import type {TBShopSimpleResponse} from "~src/columns/TBShopSimple";
+import type {TBShopSimpleItemResponse} from "~src/columns/TBShopSimpleItem";
 
 export const config: PlasmoCSConfig = {
   matches: ["*://*.taobao.com/*"],
   world: "MAIN"
 }
 
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+type EventAttachInfo = {
+  detail: { type: 'ITEM_LIST' | 'SHOP_INFO'; responseText: string }
+};
+
+export type ApiResponseType = 'ITEM_LIST' | 'SHOP_INFO';
+
+export type ApiResponse = TBShopSimpleResponse | TBShopSimpleItemResponse;
+
+const onMessageListener = async (e: Prettify<Event & EventAttachInfo>) => {
+  const type: ApiResponseType = e.detail.type
+  let response: ApiResponse;
+  if (type === 'ITEM_LIST') {
+    response = JSON.parse(e.detail.responseText) as TBShopSimpleResponse;
+  }
+  if (type === 'SHOP_INFO') {
+    response = JSON.parse(e.detail.responseText) as TBShopSimpleItemResponse;
+  }
+  console.log(response.api)
+}
+
 const TestPage = () => {
-  return <div>
-    Hello world
+
+  useEffect(() => {
+    window.addEventListener("FROM_INJECTED", onMessageListener, false)
+    return () => {
+      window.removeEventListener("FROM_INJECTED", onMessageListener)
+    }
+  }, [])
+
+  return <div style={{top: '220px', left: '20px', position: 'relative'}}>
+    <button style={{padding: "2px 4px"}}>show</button>
   </div>
 }
 
