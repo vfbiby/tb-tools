85e94b7112f715398a5fe88d972c0900ceba8b44
James Zhang <liulfbaby@gmail.com>
2024-09-18 15:56:46 +0800

    test auto load

---
+++ src/contents/taobao-float.tsx
@@ -0,0 +1,11 @@
+import type {PlasmoCSConfig} from "plasmo";
+
+export const config: PlasmoCSConfig = {
+  matches: ["*://*.taobao.com/*"]
+}
+
+const TaobaoFloat = () => {
+  return <div>hello james</div>
+}
+
+export default TaobaoFloat
